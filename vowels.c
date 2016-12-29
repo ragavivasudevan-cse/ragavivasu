e546c908a577d0f2a1d294fcd2985cb942c57ecf
ragavivasudevan-cse <ragavivasudevan04@gmail.com>
2016-12-29 09:45:13 +0530

    Add files via upload

---
+++ vowels.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+char ch;
+printf("enter the character:");
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+printf("%c is vowel",ch);
+else
+printf("consonants");
+}
