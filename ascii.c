7ce5d2dc0f96f8cfbbaafad3e5ca18c81b1f52cc
ragavivasudevan-cse <ragavivasudevan04@gmail.com>
2016-12-29 12:21:07 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+char ch[10];
+int i;
+printf("enter the string");
+gets(ch);
+printf("ascii values");
+for(i=0;i<6;i++){
+printf("%d",ch[i]);
+}}
