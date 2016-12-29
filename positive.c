fedbe5b37b4536f6959993c38741658196c3cbfb
ragavivasudevan-cse <ragavivasudevan04@gmail.com>
2016-12-29 09:27:40 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a>0)
+printf("%d is positive",a);
+else if(a<0)
+printf("%d is nagative",a);
+else
+printf("%d is zero",a);}
