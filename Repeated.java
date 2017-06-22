c6090bad6c139b23c25714123f48bb97adf2c765
ragavivasudevan-cse <ragavivasudevan04@gmail.com>
2017-06-22 11:02:49 +0530

    Add files via upload

---
+++ Repeated.java
@@ -0,0 +1,24 @@
+package guvi;
+import java.util.Scanner;
+public class Repeated {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+        	a[i]=sc.nextInt();
+        	
+        }
+        
+        for(int i=0;i<n;i++){
+        	for(int j=i+1;j<n;j++){
+        		if(a[i]==a[j]){
+        			System.out.println(a[j]);
+        		}
+        	}
+        }
+        sc.close();
+	}
+	
+
+}
