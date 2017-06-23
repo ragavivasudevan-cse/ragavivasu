fbd46c90ae68a00b6a2e8f921e76664254cd5b61
ragavivasudevan-cse <ragavivasudevan04@gmail.com>
2017-06-23 15:15:24 +0530

    Add files via upload

---
+++ Sort.java
@@ -0,0 +1,18 @@
+package program;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Sort {
+public static void main(String[] args){
+	Scanner s=new Scanner(System.in);
+  int n=s.nextInt();
+  int[] a=new int[n];
+  for(int i=0;i<n;i++){
+	  a[i]=s.nextInt();
+	  }
+  Arrays.sort(a);
+  for(int i=0;i<n;i++){
+	  System.out.println(a[i]);
+	  }
+  s.close();
+}
+}
