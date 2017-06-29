0047f69758de19c5d38cc0e6d372d57424a6076b
ragavivasudevan-cse <ragavivasudevan04@gmail.com>
2017-06-29 11:54:33 +0530

    Add files via upload

---
+++ Player1.java
@@ -0,0 +1,10 @@
+package guvi;
+import java.util.Scanner;
+public class Player1 {
+public static void main(String[] args){
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	System.out.println(n/2);
+	s.close();
+}
+}
