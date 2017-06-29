eebf9afc6a58929c74f11206fc78079fbd216773
ragavivasudevan-cse <ragavivasudevan04@gmail.com>
2017-06-29 11:48:09 +0530

    Add files via upload

--- Subset.java
+++ Subset.java
@@ -1,34 +1,15 @@
-package program;
+package guvi;
 import java.util.Scanner;
 public class Subset {
-	public static void main(String[] args){
-Scanner sc=new Scanner(System.in);
-int n=sc.nextInt();
-int n1=sc.nextInt();
-int count=0;
-int[] a1=new int[n];
-int[] a2=new int[n1];
-for(int i=0;i<n;i++){
-	a1[i]=sc.nextInt();
-}
-for(int i=0;i<n1;i++){
-	a2[i]=sc.nextInt();
-}
-for(int i=0;i<n;i++){
-	for(int j=0;j<n1;j++){
-	if(a1[i]==a2[j]){
-		count++;
+public static void main(String[] args){
+	Scanner s=new Scanner(System.in);
+	String s1=s.next();
+	String s2=s.next();
+	if(s1.contains(s2)){
+		System.out.println(s1.indexOf(s2.charAt(0)));
+	}else{
+		System.out.println("-1");
 	}
-}
-}
-	if(count==a1.length){
-		System.out.println("a1 is subset of a2");
-	}
-	else{
-		System.out.println("a1 is not subset of a2");
-
-	}
-
-sc.close();
+	s.close();
 }
 }
