0494ce7f8eea17d50e91c6b03c005c6f79462a93
ragavivasudevan-cse <ragavivasudevan04@gmail.com>
2017-07-28 22:42:39 +0530

    Add files via upload

---
+++ KRotate.java
@@ -0,0 +1,23 @@
+package program;
+import java.util.Scanner;
+public class KRotate {
+public static void main(String[] args){
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	int[] a=new int[n];
+	int[] b=new int[n];
+	for(int i=0;i<n;i++){
+		a[i]=s.nextInt();
+	}
+	for(int i=0;i<n;i++){
+		b[i]=s.nextInt();
+	}
+	for(int i=0;i<n;i++){
+		if(b[0]==a[i]){
+			System.out.println(a[i]);
+		}
+	}
+	s.close();
+	
+}
+}
