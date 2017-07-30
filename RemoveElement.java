f8f748e925b793ece70c2db63f76ad4324224b25
ragavivasudevan-cse <ragavivasudevan04@gmail.com>
2017-07-30 12:48:52 +0530

    Add files via upload

---
+++ RemoveElement.java
@@ -0,0 +1,20 @@
+package program;
+import java.util.Scanner;
+public class RemoveElement {
+public static void main(String[] args){
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	int[] a=new int[n];
+	for(int i=0;i<n;i++){
+		a[i]=s.nextInt();
+	}
+	int num=s.nextInt();
+	for(int i=0;i<n;i++){
+		if(num!=a[i]){
+			
+		System.out.println(a[i]);
+	}
+	s.close();
+}
+}
+}
